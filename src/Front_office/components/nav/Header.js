dfc19a7b543591fd1dc3dd6d5118cb0657e621e6
Robuste Manohisafidy <robustemmanuel@gmail.com>
2022-08-14 16:28:54 +0000

    Remove yarn

--- src/Front_office/components/nav/Header.js
+++ src/Front_office/components/nav/Header.js
@@ -68,9 +68,17 @@ const Header = ({handleChangeLng}) => {
       <div className="menu">
         <Link to="/"><img src={Logo} alt="logo" /></Link>
         <div className="right">
-          <div>
-            <button onClick={() =>  handleChangeLng("en")}>EN</button>
-            <button onClick={() =>  handleChangeLng("jp")}>Jp</button>
+          <div className="btn">
+            <button onClick={() =>  handleChangeLng("en")}>
+              <img src="https://imgs.search.brave.com/Zt7ekJvP8Sn_NEcLzmXD1ZWh4NwWDDcMMClohDwIJWg/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9ub2Rl/MDEuZmxhZ3N0YXQu/bmV0L21lZGlhL2Nh/dGFsb2cvcHJvZHVj/dC9kZXRhaWwvcGV0/aXQtZHJhcGVhdS1y/b3lhdW1lLXVuaS0x/Mjg3LmpwZw"
+              alt="EN"
+              />
+            </button>
+            <button onClick={() =>  handleChangeLng("fr")}>
+              <img src="https://imgs.search.brave.com/qkeC6G98-j4Bo7aze5WWvwu7LWSld5AfCvpezZ6tC_0/rs:fit:1200:962:1/g:ce/aHR0cDovL25vZGUw/MS5mbGFnc3RhdC5u/ZXQvbWVkaWEvaW1h/Z2UvMjU3NHIuanBn"
+              alt="FR"
+              />
+            </button>
           </div>
           <Link to="/login">Se connecter</Link>
           <Link to="/register">S'inscrire</Link>
