868bf5f64559a5d691c61f1c8ecf65b5ae92db8b
Robuste Manohisafidy <robustemmanuel@gmail.com>
2022-08-20 11:30:38 +0300

    R

--- src/Back_office/components/cards/Top3deconsomation.js
+++ src/Back_office/components/cards/Top3deconsomation.js
@@ -1,24 +1,26 @@
 import React,{Fragment} from "react"
 import { useSelector } from "react-redux";
 import Chevron from "../icons/ChevronDown"
+import avatar1 from "../../../assets/avatar.jpg"
+import avatar2 from "../../../assets/avatar2.jpg"
+import avatar3 from "../../../assets/Avatar.webp"
 
 const mostOrdered = [
     {
-      name: "David",
-      image: "https://avatars.githubusercontent.com/u/82655694?v=4"
+      name: "John",
+      image: avatar1
     },
     {
-      name: "Goliata",
-      image: "https://avatars.githubusercontent.com/u/93115585?v=4"
+      name: "Jane",
+      image: avatar2
     },
     {
-      name: "Solomon",
-      image: "https://avatars.githubusercontent.com/u/80751503?s=400&u=6a0d04a90a1089e5ad180560b65371d56c0a20a8&v=4"
+      name: "Manohisafidy",
+      image: avatar3
     },
   ];
 
 const MostOrdered = ()=>{
-    const allStudents = useSelector(state=>state.etudiant)
     return(
         <Fragment>
             <div className="flex flex-col p-6 bg-gray-900 rounded-lg gap-y-6">
@@ -27,17 +29,15 @@ const MostOrdered = ()=>{
                 </div>
                 <hr className="border-gray-700" />
                 <div className="flex flex-col gap-y-4">
-                    {allStudents.length>=3 ?
-                    allStudents
-                    .slice(0,3)
+                    {mostOrdered
                     .map((order)=>(
                         <div  className="flex gap-x-4 items-center">
-                            <img className="w-14 h-14 rounded-full" src={order.image.replace("http://127.0.0.1:6000","https://wifizara-back.iteam-s.mg")}  alt="" />
+                            <img className="w-14 h-14 rounded-full" src={order.image}  alt="" />
                             <div className="flex flex-col gap-y-0.5">
-                            <div className="text-sm font-medium text-white">{ order.lastName }</div>
+                            <div className="text-sm font-medium text-white">{ order.name }</div>
                          </div>
                         </div>
-                    )):null}
+                    ))}
                 </div>
             </div>
         </Fragment>
